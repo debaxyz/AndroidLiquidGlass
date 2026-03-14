3a3c0a287465abdb45f05717bc8c112ea491d788
debaxyz <debaxyz3@gmail.com>
2026-03-14 19:29:17 +0530

    Upload

--- backdrop/build.gradle.kts
+++ backdrop/build.gradle.kts
@@ -1,34 +1,37 @@
 plugins {
-    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    id("com.vanniktech.maven.publish")
+    alias(libs.plugins.android.library)
+    
+    id("maven-publish")
+    id("signing")
 }
 
 android {
     namespace = "com.kyant.backdrop"
-    compileSdk {
-        version = release(36)
-    }
-    buildToolsVersion = "36.1.0"
+    compileSdk = 36
 
     defaultConfig {
         minSdk = 21
-        consumerProguardFiles("consumer-rules.pro")
+        targetSdk = 36
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-        }
-    }
     buildFeatures {
         compose = true
+    }
+
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+            withJavadocJar()
+        }
     }
 }
 
 kotlin {
-    jvmToolchain(21)
+    jvmToolchain(17)
     compilerOptions {
         freeCompilerArgs.addAll(
             "-Xcontext-parameters"
@@ -37,41 +40,78 @@ kotlin {
 }
 
 dependencies {
+    implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.foundation)
-    implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.graphics)
-    implementation(libs.kyant.shapes)
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.debaxyz.shapes)
+}
+// Configure publishing
+publishing {
+    publications {
+        create<MavenPublication>("release") {
+            // Maven coordinates: groupId:artifactId:version
+            groupId = "io.github.debaxyz"           // Your group ID
+            artifactId = "backdrop"                  // Your artifact ID
+            version = "1.0.0"                         // Library version
+
+            afterEvaluate {
+                from(components["release"])
+            }
+            
+            pom {
+               name.set("Backdrop")
+               description.set("Android Compose Liquid Glass effect.")
+               inceptionYear.set("2026")
+               url.set("https://github.com/debaxyz/AndroidLiquidGlass/")
+           licenses {
+           license {
+              name.set("The Apache License, Version 2.0")
+              url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+              distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+           developers {
+           developer {
+             id.set("debaxyz")
+             name.set("Deba")
+             url.set("https://github.com/AndroidLiquidGlass/")
+      }
+    }
+    scm {
+      url.set("https://github.com/debaxyz/debaxyz/AndroidLiquidGlass/")
+      connection.set("scm:git:git://github.com/debaxyz/AndroidLiquidGlass.git")
+      developerConnection.set("scm:git:ssh://git@github.com/debaxyz/AndroidLiquidGlass.git")
+      }
+    }
+  } 
 }
 
-mavenPublishing {
-    publishToMavenCentral()
-    signAllPublications()
-
-    coordinates("io.github.kyant0", "backdrop", "1.0.6")
-
-    pom {
-        name.set("Backdrop")
-        description.set("Jetpack Compose blur and Liquid Glass effects")
-        inceptionYear.set("2025")
-        url.set("https://github.com/Kyant0/AndroidLiquidGlass")
-        licenses {
-            license {
-                name.set("The Apache License, Version 2.0")
-                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                distribution.set("repo")
+    repositories {
+    
+        maven {
+            name = "GitHubPackages"
+            // Format: https://maven.pkg.github.com/OWNER/REPOSITORY
+            url = uri("https://maven.pkg.github.com/debaxyz/AndroidLiquidGlass")
+            
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_USER")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
             }
         }
-        developers {
-            developer {
-                id.set("Kyant0")
-                name.set("Kyant")
-                url.set("https://github.com/Kyant0")
-            }
-        }
-        scm {
-            url.set("https://github.com/Kyant0/AndroidLiquidGlass")
-            connection.set("scm:git:git://github.com/Kyant0/AndroidLiquidGlass.git")
-            developerConnection.set("scm:git:ssh://git@github.com/Kyant0/AndroidLiquidGlass.git")
+        
+        maven {
+            val releasesRepoUrl = layout.buildDirectory.dir("repos/releases")
+            val snapshotsRepoUrl = layout.buildDirectory.dir("repos/snapshots")
+            url = uri(if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl)
         }
     }
+}
+
+// Signing configuration - moved after publishing and uses lazy evaluation
+signing {
+    sign(publishing.publications["release"])
+}
+tasks.withType<PublishToMavenRepository>().configureEach {
+    dependsOn(tasks.withType<Sign>())
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,27 +1,23 @@
 pluginManagement {
     repositories {
-        google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
-            }
-        }
+        google()
         mavenCentral()
         gradlePluginPortal()
     }
 }
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
-}
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+        
     }
 }
 
-rootProject.name = "Glass"
-include(":backdrop")
+rootProject.name = "AndroidLiquidGlass"
+
 include(":catalog")
+include(":backdrop")
+
+
