f548bbaaa7cb02d640bea9ee9dce3e7cf22c292f
debaxyz <debaxyz3@gmail.com>
2026-03-14 19:35:16 +0530

    Update

--- backdrop/build.gradle.kts
+++ backdrop/build.gradle.kts
@@ -3,8 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.android.library)
     
-    id("maven-publish")
-    id("signing")
+    
 }
 
 android {
@@ -45,73 +44,4 @@ dependencies {
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.debaxyz.shapes)
-}
-// Configure publishing
-publishing {
-    publications {
-        create<MavenPublication>("release") {
-            // Maven coordinates: groupId:artifactId:version
-            groupId = "io.github.debaxyz"           // Your group ID
-            artifactId = "backdrop"                  // Your artifact ID
-            version = "1.0.0"                         // Library version
-
-            afterEvaluate {
-                from(components["release"])
-            }
-            
-            pom {
-               name.set("Backdrop")
-               description.set("Android Compose Liquid Glass effect.")
-               inceptionYear.set("2026")
-               url.set("https://github.com/debaxyz/AndroidLiquidGlass/")
-           licenses {
-           license {
-              name.set("The Apache License, Version 2.0")
-              url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-              distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-            }
-        }
-           developers {
-           developer {
-             id.set("debaxyz")
-             name.set("Deba")
-             url.set("https://github.com/AndroidLiquidGlass/")
-      }
-    }
-    scm {
-      url.set("https://github.com/debaxyz/debaxyz/AndroidLiquidGlass/")
-      connection.set("scm:git:git://github.com/debaxyz/AndroidLiquidGlass.git")
-      developerConnection.set("scm:git:ssh://git@github.com/debaxyz/AndroidLiquidGlass.git")
-      }
-    }
-  } 
-}
-
-    repositories {
-    
-        maven {
-            name = "GitHubPackages"
-            // Format: https://maven.pkg.github.com/OWNER/REPOSITORY
-            url = uri("https://maven.pkg.github.com/debaxyz/AndroidLiquidGlass")
-            
-            credentials {
-                username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_USER")
-                password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
-            }
-        }
-        
-        maven {
-            val releasesRepoUrl = layout.buildDirectory.dir("repos/releases")
-            val snapshotsRepoUrl = layout.buildDirectory.dir("repos/snapshots")
-            url = uri(if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl)
-        }
-    }
-}
-
-// Signing configuration - moved after publishing and uses lazy evaluation
-signing {
-    sign(publishing.publications["release"])
-}
-tasks.withType<PublishToMavenRepository>().configureEach {
-    dependsOn(tasks.withType<Sign>())
 }
